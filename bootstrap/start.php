680277930acc6ee0951d10b7af13d96709fdf237
Ah-Lun Tang <tang@ahlun.be>
2013-07-05 15:39:03 +0200

    setting local environment
    Signed-off-by: Ah-Lun Tang <tang@ahlun.be>

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,8 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+    'local' => array('*.dev', 'localhost'),
+    'production' => array('your-machine-name'),
 
 ));
 
