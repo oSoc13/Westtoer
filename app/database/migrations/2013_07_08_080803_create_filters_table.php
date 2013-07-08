f2b804246c239e693d5031dd96b94f87599feb08
Ah-Lun Tang <tang@ahlun.be>
2013-07-08 11:35:26 +0200

    Issue #2 : base models for Filter and Screen.
    Signed-off-by: Ah-Lun Tang <tang@ahlun.be>

--- app/database/migrations/2013_07_08_080803_create_filters_table.php
+++ app/database/migrations/2013_07_08_080803_create_filters_table.php
@@ -4,41 +4,49 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateFiltersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('filters', function($table)
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('filters', function($table)
         {
             $table->create();
             $table->timestamps();
             $table->increments('id');
             // type of content: Event, Attraction,...
+            $contenttypes = array(
+                Connection::CAPTURED,
+                Connection::PENDING,
+                Connection::ACTIVE,
+                Connection::CLOSED,
+            );
+ 
+            $table->enum('type', $contenttypes)->default($contenttypes[0]);
             $table->string('type'); 
             
             $table->integer('screen_id'); 
-            
+
             // simple scoring
-            //	-99: exclude
-            //	 -1: less important
-            //	  0: not rated
-            //	  1: important
+            //  -99: exclude
+            //   -1: less important
+            //    0: not rated
+            //    1: important
             $table->integer('score');
 
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::drop('filters');
-	}
+    }
 
 }
---
+++ app/models/Filter.php
@@ -0,0 +1,13 @@
+<?php
+
+class Filter extends Eloquent{
+
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'filters';
+
+}
