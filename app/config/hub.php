c2cd50e8850947669bed32179f46cdcbbd060901
Ah-Lun Tang <tang@ahlun.be>
2013-07-08 17:11:06 +0200

    Updated config
    Signed-off-by: Ah-Lun Tang <tang@ahlun.be>

--- app/config/hub.php
+++ app/config/hub.php
@@ -1,7 +1,8 @@
 <?php 
 
 return array(
-	'user' => '', // username for datahub
-	'password' => '', // password for datahub
+	'base_url' => 'https://datahub.dev/', // location of the datahub server
+	'user' => 'username', // username for datahub
+	'password' => 'password', // password for datahub
 	'cache_ttl' => 60*60 // set cache time to live
 );
