d5ee51620ce08d4661e6a28b4977a6649028e1d3
Ah-Lun Tang <tang@ahlun.be>
2013-07-13 22:10:41 +0200

    Added example config for weather and removed duplicate code from weathercontroller.
    Signed-off-by: Ah-Lun Tang <tang@ahlun.be>

--- app/controllers/WeatherController.php
+++ app/controllers/WeatherController.php
@@ -2,7 +2,6 @@
 
 class WeatherController extends \BaseController {
 
-    private $ttl = 1800;
     private $screen;
 
 
@@ -32,55 +31,6 @@ class WeatherController extends \BaseController {
             Cache::section('screen_weather')->put($id, $parsed_weather, 60);
             return $parsed_weather;
         }
-    }
-
-
-    private function getList(){
-        //TODO: remove providers, only one getEvent needed.
-        $win_events     = $this->getEvents('WIN');
-        $uitdb_events   = $this->getEvents('UITDB');
-        $events         = array_merge($win_events, $uitdb_events);
-        $matched_events = $this->matchFilters($events);
-        Cache::section('matched_events')->put($this->screen->id, $matched_events, 60);
-        return $matched_events;
-    }
-
-    //TODO: remove provider when datahub is completed
-    private function getEvents($provider = 'WIN', $limit = -1) // UITDB or WIN
-    {
-        if ($events = Cache::section('origin')->get('events_parsed'))
-        {
-            return $events;
-        } 
-        else
-        {
-            $raw_events = Hub::get($provider."/Events.json");
-
-            $events = EventParser::getEvents($raw_events);
-            Cache::section('origin')->put('events_parsed', $events, $this->ttl);
-
-            return $events;
-        }
-    }
-
-
-    private function matchFilters($events){
-        $filters = $this->screen->filters();
-        $matched_events;
-
-        // for each event, find corresponding filter.
-        foreach ($events as $key => $event) {
-            $found = false;
-            if($filter = $this->screen->filters()->where('item_id', $event->name)->first()){
-                $event->score = $filter->score;
-            } else {
-                $event->score =0;
-            }
-            if($event->score > -1){
-                $matched_events[$event->name] = $event;
-            }
-        }
-        return $matched_events;
     }
 
 }
